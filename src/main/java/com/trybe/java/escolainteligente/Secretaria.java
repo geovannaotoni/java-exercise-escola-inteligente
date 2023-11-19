a4616da8a7c60a1696bb2da5cdb0d8288cdedbd5
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-19 14:40:20 -0300

    feat02: classe Secretaria e métodos calcularMedia e coletarInformacoes

--- src/main/java/com/trybe/java/escolainteligente/Secretaria.java
+++ src/main/java/com/trybe/java/escolainteligente/Secretaria.java
@@ -8,14 +8,28 @@ public class Secretaria {
    * Método calcularMedia.
    */
   public static double calcularMedia(double nota1, double nota2, double nota3, double nota4) {
-    System.out.println("TODO: Implementar a função calcular média.");
-    return 0;
+    return (nota1 + nota2 + nota3 + nota4) / 4;
   }
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações para cálculo da média ");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Qual o nome da Pessoa Estudante?");
+    String nome = scanner.next();
+    System.out.println("Entre com as notas das provas:");
+    System.out.println("Avaliação 1:");
+    double av1 = Double.parseDouble(scanner.next());
+    System.out.println("Avaliação 2:");
+    double av2 = Double.parseDouble(scanner.next());
+    System.out.println("Avaliação 3:");
+    double av3 = Double.parseDouble(scanner.next());
+    System.out.println("Avaliação 4:");
+    double av4 = Double.parseDouble(scanner.next());
+
+    double media = calcularMedia(av1, av2, av3, av4);
+    System.out.println("A média das notas de " + nome + " é " + media);
+    scanner.close();
   }
 }
